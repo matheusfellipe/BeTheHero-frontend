e9e75658da161c352ca8577330061a30f7a272a3
Matheus Fellipe <matheus060399@gmail.com>
2021-11-28 14:27:53 -0400

    pages responsives

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import {FiLogIn} from 'react-icons/fi';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import api from '../../services/api';
 
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.png';
 
 export default function Logon(){
+    const [id,setId] = useState('');
+    const navigate = useNavigate();
+
+    async function HandLogin (e){
+        e.preventDefault();
+        try {
+
+        const response = await api.post('sessions', {id});
+
+           localStorage.setItem('ongId',id);
+           localStorage.setItem('ongName',response.data.name);
+
+           navigate('/profile');
+        }
+        catch (err){
+            alert('Falha no Login, tente novamente.')
+        }
+    }
+
     return (
      <div className="logon-container">
          <section className="form">
          <img src={logoImg} alt="Be The Hero"/>
-         <form>
+         <form onSubmit={HandLogin}>
              <h1>Faça o seu Logon</h1>
-             <input placeholder="Sua ID" />
+             <input placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}             
+             />
             <button className="button" type="submit">Entrar</button>
 
 
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import './style.css';
-import { Link } from "react-router-dom";
-import { FiArrowLeft } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
+import api from '../../services/api';
 
-import logoImg from '../../assets/logo.png'
+import { FiArrowLeft } from "react-icons/fi";
+import logoImg from '../../assets/logo.png';
 
 export default function Register(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+    const navigate = useNavigate();
+
+    async function handleRegister (e){
+        e.preventDefault();
+        const data =
+            {
+                name,
+                email,
+                whatsapp,
+                city,
+                uf,
+            };
+
+        try {
+           const response = await api.post('ongs',data);
+           alert(`Seu ID de acesso: ${response.data.id}`);
+
+           navigate('/');
+        }
+        catch (err){
+            alert('Erro no cadastro, tente novamente')
+        }
+    }
     return (
         <div className="register-container">
             <div className="content">
@@ -21,14 +51,29 @@ export default function Register(){
                     </Link>
                 </section>
 
-                <form >
-                    <input placeholder="nome da ONG" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="whatsapp" />
+                <form onSubmit={handleRegister}>
+                    <input placeholder="nome da ONG"
+                    value={name}
+                    onChange={e => setName(e.target.value)} 
+                    />
+                    <input type="email" placeholder="E-mail" 
+                     value={email}
+                     onChange={e => setEmail(e.target.value)} 
+                    />
+                    <input placeholder="whatsapp" 
+                     value={whatsapp}
+                     onChange={e => setWhatsapp(e.target.value)} 
+                    />
 
                     <div className = "input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{width:80}}/>
+                        <input placeholder="Cidade" 
+                         value={city}
+                         onChange={e => setCity(e.target.value)} 
+                        />
+                        <input placeholder="UF" style={{width:80}}
+                         value={uf}
+                         onChange={e => setUf(e.target.value)} 
+                        />
                     </div>
                     <button className="button" type="submit">Cadastrar</button>
 
